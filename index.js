30b7fea2b5a8e3e236d19c0c36ab96a6edc631f2
Katie Rose <katiefelten@gmail.com>
2019-08-07 17:51:39 -0400

    :ambulance: updated routing setup

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-const server = require("./server.js");
+const express = require("express");
+
+const server = express();
+const postRouter = require("./posts/postRouter");
+const userRouter = require("./users/userRouter");
+const serverRouter = require("./server");
+
+server.use(express.json());
+
+server.use("/", serverRouter);
+server.use("/api/users", userRouter);
+server.use("/api/posts", postRouter);
+
 
 server.listen(8000, () => {
   console.log("server running on 8000");
--- server.js
+++ server.js
@@ -1,14 +1,17 @@
-const express = 'express';
-const server = express();
+const express = "express";
+const server = require("express").Router();
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
+server.use(logger);
+
+server.get("/", (req, res) => {
+  res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
 function logger(req, res, next) {
-
-};
+  res.status(200).json({ api: "the logger is working" });
+    next();
+}
 
 module.exports = server;
