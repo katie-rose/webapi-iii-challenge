8575076fa4adcb7c3eabc106a66353f3d6d0c7ee
Katie Rose <katiefelten@gmail.com>
2019-08-07 18:17:09 -0400

    :rocket: added validateUserId mw and a few end points

--- users/userRouter.js
+++ users/userRouter.js
@@ -2,13 +2,38 @@ const express = "express";
 const db = require("./userDb");
 const router = require("express").Router();
 
-router.post("/", (req, res) => {});
+router.post("/", validateUser, (req, res) => {
+  db.insert(req.body)
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(err => {
+      res.status(500).json({ error: "error adding user" });
+    });
+});
 
 router.post("/:id/posts", (req, res) => {});
 
-router.get("/", (req, res) => {});
+router.get("/", (req, res) => {
+  db.get()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      res.status(500).json({ error: "error retrieving users" });
+    });
+});
 
-router.get("/:id", (req, res) => {});
+router.get("/:id", validateUserId, (req, res) => {
+  const id = req.params.id;
+  db.getById(id)
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(err => {
+      res.status(500).json({ error: "error retrieving user" });
+    });
+});
 
 router.get("/:id/posts", (req, res) => {});
 
@@ -18,7 +43,23 @@ router.put("/:id", (req, res) => {});
 
 //custom middleware
 
-function validateUserId(req, res, next) {}
+function validateUserId(req, res, next) {
+  const id = req.params.id;
+  db.getById(id)
+    .then(user => {
+      if (user) {
+        req.user = user;
+        next();
+      } else {
+        res.status(400).json({ message: "invalid user id" });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: "There was an error accessing that user from the database."
+      });
+    });
+}
 
 function validateUser(req, res, next) {}
 
